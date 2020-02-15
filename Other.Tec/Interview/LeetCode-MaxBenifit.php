238720678664b0313d4a1fd9f305de650551e378
WuLihua <1286045531@qq.com>
2020-02-15 20:56:22 +0800

    Feat: 新增面试笔记  -新增面试笔记

--- Other.Tec/Interview/LeetCode-MaxBenifit.php
+++ Other.Tec/Interview/LeetCode-MaxBenifit.php
@@ -8,7 +8,7 @@
  * 给你一个股价序列，告诉你每个时间点的股价，问你什么时候买什么时候卖获利最大。时间复杂度越低越好。
  * 时间复杂度: O(N)
  */
-function getMaxBenifit($arr) {
+function getMaxBenefit($arr) {
 	if (empty($arr)) {
 		return [];
 	}
@@ -16,20 +16,22 @@ function getMaxBenifit($arr) {
 	if ($arr_len < 2) {
 		return [];
 	}
-	$price_min = min($arr[0], $arr[1]);
-	$max_benifit = $arr[1] - $arr[0];
-	for ($i = 2; $i < $arr_len; $i ++) {
-		if ($arr[$i] - $price_min > $max_benifit) {
-			$max_benifit = $arr[$i] - $price_min;
-		}
+	$price_min = $arr[0];
+	$max_benefit = 0;
+	for ($i = 1; $i < $arr_len; $i ++) {
 		if ($arr[$i] < $price_min) {
 			$price_min = $arr[$i];
+			continue;
+		} else {
+			$currentBenefit = $arr[$i] - $price_min;
+			if ($currentBenefit > $max_benefit) {
+				$max_benefit = $currentBenefit;
+			}
 		}
 	}
-
-	return $max_benifit;
+	return $max_benefit;
 }
 
 $arr = [1, 1.2, 2, 4, 5, 1, 9];
-$res = getMaxBenifit($arr);
+$res = getMaxBenefit($arr);
 var_dump($res);
